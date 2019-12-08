8dcdc475dbbd86c814c98d1a952826647c7153b6
nvm98 <32503780+nvm98@users.noreply.github.com>
2019-12-08 18:46:25 +0700

    commit first

---
+++ ManhReactMusic/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+} from 'react-native';
+
+import Header from './app/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header message="Playing" />
+      </View>
+    );
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(4,4,4)',
+  },
+}
